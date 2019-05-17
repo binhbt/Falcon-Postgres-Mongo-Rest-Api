2b955158cd93f56c5f1ef1a46ed7b74a4e3d65b4
binhbt <thanhbinh.gd@gmail.com>
2019-05-17 17:10:28 +0700

    add docker

--- app/config.py
+++ app/config.py
@@ -20,22 +20,24 @@ INI_FILE = os.path.join(
 CONFIG = configparser.ConfigParser()
 CONFIG.read(INI_FILE)
 POSTGRES = CONFIG['postgres']
-if APP_ENV == 'dev' or APP_ENV == 'live':
-    DB_CONFIG = (POSTGRES['user'], POSTGRES['password'], POSTGRES['host'], POSTGRES['database'])
-    DATABASE_URL = "postgresql+psycopg2://%s:%s@%s/%s" % DB_CONFIG
-
-else:
-    DB_CONFIG = (POSTGRES['host'], POSTGRES['database'])
-    DATABASE_URL = "postgresql+psycopg2://%s/%s" % DB_CONFIG
+# if APP_ENV == 'dev' or APP_ENV == 'live':
+#     DB_CONFIG = (POSTGRES['user'], POSTGRES['password'], POSTGRES['host'], POSTGRES['database'])
+#     DATABASE_URL = "postgresql+psycopg2://%s:%s@%s/%s" % DB_CONFIG
+#
+# else:
+#     DB_CONFIG = (POSTGRES['host'], POSTGRES['database'])
+#     DATABASE_URL = "postgresql+psycopg2://%s/%s" % DB_CONFIG
 
 DB_ECHO = True if CONFIG['database']['echo'] == 'yes' else False
+DATABASE_URL = "postgresql+psycopg2://test:test@postgres/test"
+
 DB_AUTOCOMMIT = True
 
 LOG_LEVEL = CONFIG['logging']['level']
 
 MONGO = {
     'DATABASE': 'database-1',
-    'HOST': 'localhost',
+    'HOST': '192.168.1.27',
     'PORT': 27017,
     'USERNAME': '',
     'PASSWORD': ''
